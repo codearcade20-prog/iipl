41c9454298647c6856a013dcac324afdc8191741
NANDAKUMAR-1020 <vh12964_ai23@velhightech.com>
2026-01-02 23:14:47 +0530

    configure for github pages deployment

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AdminDashboard from './pages/AdminDashboard';
 import PaymentRequest from './pages/PaymentRequest';
